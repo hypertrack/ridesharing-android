136ec1f368a5d3db0701bb57d7b1e2ad9b225d45
Denys <quux@ukr.net>
2020-07-01 21:19:53 +0300

    Unused method removed

--- app/src/main/java/com/hypertrack/ridesharing/components/driver/DriverMapPresenter.java
+++ app/src/main/java/com/hypertrack/ridesharing/components/driver/DriverMapPresenter.java
@@ -17,7 +17,6 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -319,21 +318,6 @@ public class DriverMapPresenter extends MapPresenter<DriverMapPresenter.DriverVi
                 }
             }
             mView.updateNotifications(newOrders);
-        }
-    }
-
-    private void updateMapCamera() {
-        if (!mState.newOrders.isEmpty()) {
-            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            if (mState.currentLocation != null) {
-                builder.include(
-                        new LatLng(mState.currentLocation.getLatitude(), mState.currentLocation.getLongitude())
-                );
-            }
-            for (Order order : mState.newOrders.values()) {
-                builder.include(order.pickup.getLatLng());
-            }
-            animateCamera(builder.build());
         }
     }
 
